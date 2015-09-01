f5c3e86b11d095f377e45b07b7b9880d8e3c1d70
Will Schipp <will.schipp@gmail.com>
2015-09-01 11:42:07 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Hello World App
+
+simple Spring Boot App for PCF installation verification.
+
+useful for verifying standard, RBDMS and other configurations
+
+#### Branch
+
+- branch 'rdbms' is for verification of installed rdbms services
+- branch 'redis' is for verification of RedisConnectionFactory support 
---
+++ src/main/java/com/emc/awg/endpoint/HelloEndpoint.java
@@ -0,0 +1,27 @@
+package com.emc.awg.endpoint;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloEndpoint {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String sayHello()  {
+		return "hello world";
+	}
+	
+	@RequestMapping(value="datasource",method=RequestMethod.GET)
+	public boolean hasDatabaseConnection() throws Exception {
+		return dataSource.getConnection().isClosed();
+	}
+	
+}
