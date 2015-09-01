95a44e443b5e694230d118877944abe1cf0f4a4d
Will Schipp <will.schipp@gmail.com>
2015-09-01 11:42:51 +0400

    simple HTTP endpoint

--- src/main/java/com/emc/awg/endpoint/HelloEndpoint.java
+++ src/main/java/com/emc/awg/endpoint/HelloEndpoint.java
@@ -1,8 +1,5 @@
 package com.emc.awg.endpoint;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloEndpoint {
-
-	@Autowired
-	private DataSource dataSource;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String sayHello()  {
 		return "hello world";
-	}
-	
-	@RequestMapping(value="datasource",method=RequestMethod.GET)
-	public boolean hasDatabaseConnection() throws Exception {
-		return dataSource.getConnection().isClosed();
 	}
 	
 }
