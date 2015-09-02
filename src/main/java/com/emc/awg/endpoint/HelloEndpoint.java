3b7efd9a720244a92bd46349ad3c48d7477e1f2e
Will Schipp <will.schipp@gmail.com>
2015-09-02 17:37:11 +0400

    redis branch

--- src/main/java/com/emc/awg/endpoint/HelloEndpoint.java
+++ src/main/java/com/emc/awg/endpoint/HelloEndpoint.java
@@ -1,5 +1,7 @@
 package com.emc.awg.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloEndpoint {
 	
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String sayHello()  {
 		return "hello world";
 	}
 	
+	@RequestMapping(value="/ping",method=RequestMethod.GET)
+	public String pingRedis() {
+		return redisTemplate.getConnectionFactory().getConnection().ping();
+	}
 }
